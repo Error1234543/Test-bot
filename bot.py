aafd319136f76cb0e9f241ffe7ba734f1602e5b6
Error1234543 <ptaviyad21@gmail.com>
2026-03-02 12:25:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,8 +16,7 @@ TELEGRAM_CHANNEL = "@NEET_JEE_GUJ"
 WHATSAPP_LINK = "https://t.me/NEET_JEE_GUJ"
 
 # Owner ID (replace with your Telegram ID)
-OWNER_ID = 
-8226637107
+OWNER_ID = 8226637107
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
