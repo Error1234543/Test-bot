247bbac1ac6dbf0614e176610360bd3b897b5ed1
Error1234543 <ptaviyad21@gmail.com>
2026-05-01 09:54:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import threading
 import logging
 
 # ---------------- CONFIG ----------------
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "8585007953:AAEqP3K3_5y43YRoYc4h99Lzlg9uE-1rAHo"
 WEB_URL = "https://oldxhdjshshshs.netlify.app/"
 OWNER_ID = 8226637107
 
